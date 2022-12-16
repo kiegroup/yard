6cf68812661a675c9ba9f1fb951848df224d6eb4
tarilabs <matteo.mortari@gmail.com>
2022-12-16 21:24:57 +0100

    add Processor

---
+++ yard-kdtable/src/main/java/org/drools/yard/kdtable/KdtableProcessor.java
@@ -0,0 +1,18 @@
+package org.drools.yard.kdtable;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KdtableProcessor implements Processor {
+    private static final Logger LOG = LoggerFactory.getLogger(KdtableProcessor.class);
+
+    public static final String PROP_KEY = "yard.kdtable";
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        LOG.info("for prop key {} value {}", PROP_KEY, exchange.getContext().getPropertiesComponent().resolveProperty(PROP_KEY));
+    }
+    
+}
