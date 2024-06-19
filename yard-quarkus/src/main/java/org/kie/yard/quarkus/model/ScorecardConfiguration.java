21b2a76a3542d1102c99f0d3374cb4b80442d4d6
rikkola <toni@rikkola.net>
2024-06-19 12:45:32 +0300

    Extracting Akrivis

--- yard-quarkus/src/main/java/org/kie/yard/quarkus/model/ScorecardConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.kie.yard.quarkus.model;
-
-
-import java.util.*;
-
-public record ScorecardConfiguration(Optional<ScorecardConfiguration> parent,
-                                     Configuration configuration) {
-
-}
--- yard-quarkus/src/main/java/org/kie/yard/quarkus/model/result/Record.java
+++
@@ -1,5 +0,0 @@
-package org.kie.yard.quarkus.model.result;
-
-public record Record(String status, Number measureValue, String measureName,
-                     Number maxValue, String yaml, Threshold[] thresholds) {
-}
